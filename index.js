8d6a687aef05d6ad143277eb89c170808be1553c
manueldezman <adelekeabdulganiy@gmail.com>
2024-07-23 20:26:50 +0100

    create operate function
    the operate function calls one of the basic amthematical oparations depending on the users input.

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNumber, operator, secondNumber;
+
+function operate(firstNumber, operator, secondNumber) [
+    
+    switch(operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+        default:
+            return "error";
+    }
+]
